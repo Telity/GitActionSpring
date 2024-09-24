fff2e7d59266156e0d14d4e7efbd586c68c56b2b
Telity <Nigu0001@Stud.kea.dk>
2024-09-24 10:08:56 +0200

    Initial commit

---
+++ src/main/java/gruppe7/gitactionspring/GitActionSpringApplication.java
@@ -0,0 +1,13 @@
+package gruppe7.gitactionspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitActionSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitActionSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gruppe7/gitactionspring/GitActionSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package gruppe7.gitactionspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
