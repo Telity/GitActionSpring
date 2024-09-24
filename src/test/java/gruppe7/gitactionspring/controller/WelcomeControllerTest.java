3d3b7f8ee03f010e0ae48d307465b14861f6bed0
Telity <Nigu0001@Stud.kea.dk>
2024-09-24 11:01:19 +0200

    test

---
+++ src/test/java/gruppe7/gitactionspring/controller/WelcomeControllerTest.java
@@ -0,0 +1,39 @@
+package gruppe7.gitactionspring.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
+import static java.lang.reflect.Array.get;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(WelcomeController.class)
+class WelcomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    public void welcome() throws Exception {
+        mockMvc.perform(get("/welcome"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("welcome"));
+    }
+}
